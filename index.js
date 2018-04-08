8a48dc8a59a02aa51b5b62adbc67803d5d4be881
vanda may <fengriffin2002@hotmail.com>
2018-04-08 23:57:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  return ("I can/'t hear you") 
+  string.toLowerCase()
 }
