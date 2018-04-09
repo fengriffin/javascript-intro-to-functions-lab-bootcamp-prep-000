811d62e447ea6f60224598d848e2cc3f5cc6560c
vanda may <fengriffin2002@hotmail.com>
2018-04-09 00:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ function sayHiToGrandma(string) {
    }
   else if(string === string.toUpperCase()) { 
    return ("YES INDEED!")
-  }else{ 
+  }
+  else if(string === "I love you, Grandma."){
+    
+  }else{
     return("I love you, Grandma.")
   
   }
